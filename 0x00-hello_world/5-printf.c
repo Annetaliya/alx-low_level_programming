01440dce355bd2624e36d74149dfc527dbba260c
Annette Adhiambo <annetaliya@gmail.com>
2022-09-08 13:39:44 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - writes a string to the standard output
+ * return: the value of string
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
