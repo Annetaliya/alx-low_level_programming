7458ac3ec1f1726ffbd147f7d2f8ae60013658cd
Annette Adhiambo <annetaliya@gmail.com>
2022-09-10 12:14:05 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Display alphabets inlower case
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
