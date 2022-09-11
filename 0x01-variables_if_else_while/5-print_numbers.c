9089536e9c6bbf8344651065058475b48d045c64
Annette Adhiambo <annetaliya@gmail.com>
2022-09-11 01:13:01 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- *main - Display numbers of base 10
- *return: Always 0 (success)
+ * main- Prints numbers from 0 to 9
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 	int a = 0;
+
 	while (a < 10)
 	{
 		printf("%d", a);
