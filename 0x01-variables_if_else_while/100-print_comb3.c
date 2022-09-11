af7427bd6dd48d36b36c4768baa9655624516fce
Annette Adhiambo <annetaliya@gmail.com>
2022-09-11 04:53:32 -0700

    combination two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- Prints combination of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 100; num++)
+	{
+		putchar((num / 10) + '0');
+		putchar((num % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
