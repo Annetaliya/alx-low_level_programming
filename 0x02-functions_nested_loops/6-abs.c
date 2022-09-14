dd0cc2f10a640cfb7859aeef5097793793ed8935
Annette Adhiambo <annetaliya@gmail.com>
2022-09-14 03:51:45 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs- returns abs value of an int
+ * @a: prints value of a
+ * Return: 0
+ */
+
+int _abs(int a)
+{
+	if (a > 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (-a);
+	}
+}
