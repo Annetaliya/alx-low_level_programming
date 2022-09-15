195d08e7cfb9f0f12f738df46c809b409a2309d4
Annette Adhiambo <annetaliya@gmail.com>
2022-09-15 12:51:51 -0700

    upercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isupper - check if a character is uypper or not
+ *@c: character to be tested
+ *Return: 1 whether it is 0 or otherwise
+ *
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (0);
+	}
+	return (0);
+}
