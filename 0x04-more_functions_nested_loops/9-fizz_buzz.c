08c38a2a746cb8c99938a608af603d2baf22f7bd
Annette Adhiambo <annetaliya@gmail.com>
2022-09-16 01:18:39 -0700

    buzz fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
+		if ((num % 3) == 0 && (num % 15) == 0)
 			printf("Fizz");
 		else if ((num % 5) == 0)
 			printf("Buzz");
 
 		else
 			printf("%d", num);
-		if (num == 100)
+		if (num < 100)
 			continue;
 		printf(" ");
 	}
