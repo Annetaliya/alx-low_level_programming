66e52e49efd1d060032448b7a094afd07e493985
Annette Adhiambo <annetaliya@gmail.com>
2022-09-21 23:33:13 -0700

    converts a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *string_toupper- convert string to uppercase
+ *@s:string to manipulate
+ *Return: string with all capital letters
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
+	}
+	return (s);
+}
