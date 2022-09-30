2eaab26870054b9c348ab8ccb901b17c937c5fdd
Annette Adhiambo <annetaliya@gmail.com>
2022-09-30 06:36:32 -0700

    1-argc.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main- entry point
+ *@argc: argument count
+ *@argv: array of strings
+ *Return: always 0 success
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argv;
+
+		printf("%d\n", argc);
+	return (0);
+}
