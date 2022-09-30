357994f1b0ad7f1578d3234c6714541bc6840413
Annette Adhiambo <annetaliya@gmail.com>
2022-09-30 07:36:27 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- entry point
+ *@argc: argument count
+ *@argv: array of strings
+ *Return: error if the program does not receive two arguments
+ *and return 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int mult = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	mult *= atoi(argv[i]);
+	}
+	printf("%d\n", mult);
+	return (0);
+
+}
