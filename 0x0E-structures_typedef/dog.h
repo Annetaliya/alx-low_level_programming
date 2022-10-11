23a54b04e637b5780851ab2d831fb48124fbba01
Annette Adhiambo <annetaliya@gmail.com>
2022-10-11 05:01:01 -0700

    my struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - name of structure
+ *@name: first member
+ *@age: second member
+ *@owner: third member
+ *
+ * Description: longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
